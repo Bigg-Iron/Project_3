3b525d48204038ad5bfd901fad3a386c9828b844
Anna Ly <annatly2@gmail.com>
2018-01-20 17:43:24 -0800

    initial modal

--- client/src/components/Display/Display.js
+++ client/src/components/Display/Display.js
@@ -1,6 +1,12 @@
 import React from "react";
+import "./Display.css";
+// import Modal from "../../components/Modal";
+import { Button } from 'react-bootstrap';
 
- const Display = ({launchData, readData}) => {
+// import styles from './imagecard.less';
+// import { Row, Col, Card, Icon } from 'antd';
+
+ const Display = ({launchData, readData, openModal, closeModal}) => {
   const mission = launchData.missions[0];
   let image = launchData.rocket.imageURL
 
@@ -8,20 +14,25 @@ import React from "react";
     image = "https://s3.amazonaws.com/launchlibrary/RocketImages/placeholder_1920.png"
   }
   return(
-    <div className ="display-card">
-      <div className="col-sm-4" onClick ={ () => readData(launchData)}>
-      <div className = "display-image">
-        <img src = {image} width = "366" height = "243"/>
-      </div>
-      <div className = "display-content">
-        <div>ID: {launchData.id}</div> 
-        <div>NAME: {launchData.name}</div>
-        <div>DATE: {launchData.windowstart}</div>
-        <div>DESCRIPTION: {mission ? mission.description : "Description is unavailable."}</div>
-        <div>LAUNCHPAD: {launchData.location.pads[0].name}</div>
-        <div>latitude: {launchData.location.pads[0].latitude}</div>
-        <div>longitude: {launchData.location.pads[0].longitude}</div>
-      </div>
+      <div className="col-sm-4">
+      <div className ="display-card">
+        <div className = "display-image">
+          <img src = {image} width = "366" height = "243" alt = "launch"/>
+        </div>
+        <div className = "display-content">
+          <div className = "display-id">{launchData.id}</div> 
+          <div className = "display-name">{launchData.name}</div>
+          <div className = "display-date">{launchData.windowstart}</div>
+          {/* <div>DESCRIPTION: {mission ? mission.description : "Description is unavailable."}</div> */}
+          <div className = "display-pad">{launchData.location.pads[0].name}</div>
+          {/* <div>latitude: {launchData.location.pads[0].latitude}</div>
+          <div>longitude: {launchData.location.pads[0].longitude}</div> */}
+        </div>
+        {/* <Modal> */}
+          <button onClick ={ () => readData(launchData)}>More Info
+          </button>
+        {/* </Modal> */}
+
     </div>
   </div>
   )
--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -3,7 +3,7 @@ import "./Jumbotron.css";
 
 const Jumbotron = () => (
   <div className="jumbotron text-center">
-    <h1>Where is the next space launch?</h1>
+    <h1>When is the next space launch?</h1>
     <p>Find out when the next launch is below!</p>
   </div>
 );
